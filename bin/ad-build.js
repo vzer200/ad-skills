dcf4cc04ff8d4136bfbce9c924628a0ddc06c684
宁岑 <17338548613@163.com>
2026-06-12 20:41:01 +0800

    Add image subcommand entrypoint

--- bin/ad-build.js
+++ bin/ad-build.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
 
 const commands = require('../lib/commands');
+const baseImage = require('../lib/base-image');
 
-commands.runCli().then((exitCode) => {
+const argv = process.argv.slice(2);
+const runner = argv[0] === 'image'
+  ? baseImage.runCli(argv.slice(1))
+  : commands.runCli(argv);
+
+runner.then((exitCode) => {
   process.exitCode = exitCode;
 }).catch((error) => {
   process.stderr.write(`ad-build failed: ${error.message}\n`);
